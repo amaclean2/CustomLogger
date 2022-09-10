57609db6a4d2b44f0ca222b198790c684fa25733
Andrew <andrew.n.maclean@gmail.com>
2022-09-09 21:49:12 -0700

    logging in technicolor

---
+++ colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    'info': `\x1b[36m`,
+    'debug': `\x1b[34m`,
+    'trace': `\x1b[35m`,
+    'warn': `\x1b[33m`,
+    'error': `\x1b[31m`,
+    'fatal': `\x1b[31m`
+};
+
+module.exports = colors;
--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-exports.myLogger = () => {
-    console.log("Hi there!");
+const fs = require('fs');
+const path = require('path');
+
+const colors = require('./colors');
+
+function Logger(name, dir = "./logs", cacheSize = 100) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+
+    const timeFormatPath = new Date().toISOString().replaceAll(':', '-').split('.')[0];
+
+    const localPath = path.join(dir, `${timeFormatPath}-${name}.log`);
+
+    const cache = [];
+
+    const log = (level, message) => {
+        const dateFormat = new Date().toISOString().replace('T', '').split('.')[0];
+        const output = `${dateFormat} ${name} ${level.toUpperCase()} ${message}`;
+
+        console.log(dateFormat, name, `${colors[level]}${level.toUpperCase()}\x1b[0m`, message);
+
+        cache.push(output);
+
+        if (cache.length >= cacheSize) {
+            fs.appendFileSync(localPath, cache.map((log) => `${log}\n`).join(''));
+
+            cache = [];
+        }
+
+        return output;
+    };
+
+    const info = (message) => log('info', message)
+    const debug = (message) => log('debug', message)
+    const trace = (message) => log('trace', message)
+    const warn = (message) => log('warn', message)
+    const error = (message) => log('error', message)
+    const fatal = (message) => log('fatal', message)
+
+    const close = () => fs.appendFileSync(this.path, this.cache.map((log) => `${log}\n`).join(''));
+
+    return {
+        info,
+        debug,
+        trace,
+        warn,
+        error,
+        fatal,
+        close
+    };
 };
+
+module.exports = Logger;
