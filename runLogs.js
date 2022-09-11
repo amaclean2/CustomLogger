8b984263020559b87053cdceb6b6be2cfdb696c3
Andrew <andrew.n.maclean@gmail.com>
2022-09-11 16:57:39 -0700

    reformatting

--- runLogs.js
+++ runLogs.js
@@ -11,9 +11,9 @@ logger.info([1, 2, 3, [], 4, 5])
 logger.info({ a: 1 })
 logger.info({ a: 1, b: 2 })
 logger.info({ a: 'abc' })
-logger.info({ a: { b: 1, c: 2, d: { e: 3, f: 4 }}})
+logger.info({ a: { b: 1, c: 2, d: { e: 3, f: 4 } } })
 logger.info({})
-logger.info({ a: 1, b: {}})
+logger.info({ a: 1, b: {} })
 
 logger.debug("Hi I'm Andrew")
 
@@ -22,3 +22,29 @@ const myFun = () => 2;
 logger.trace(myFun());
 
 logger.error(new Error("Ahh!"));
+
+const data = {
+    user: {
+        id: 750,
+        first_name: 'Test',
+        last_name: 'User',
+        email: 'user@email.com',
+        is_premium: null,
+        sex: null,
+        user_site: null,
+        city: null,
+        bio: null,
+        date_created: '2022-09-11T18:31:10.000Z',
+        last_updated: '2022-09-11T18:31:10.000Z',
+        activity_count: 0,
+        follower_count: 0,
+        following_count: 0,
+        images: [],
+        ticks: []
+    },
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzUwLCJpYXQiOjE2NjI5MjEwNzAsImV4cCI6MTY2MzA5Mzg3MH0.CHa97-F16EiLlcdDhhbeg3HQcw8_49B60CrZTN4AZeg'
+};
+
+const status = 200;
+
+logger.info({ data, status });
--- write.js
+++ write.js
@@ -10,110 +10,143 @@ const writeTabs = (count) => {
 };
 
 const usePrint = (level) => {
-    return (argument, options) => {
-        if (['error', 'fatal'].includes(level.toLowerCase())) {
-            process.stderr.write(`\x1b[31m${argument}\x1b[0m`)
-        } else {
-            if (level.toLowerCase() === 'debug') {
-                process.stdout.write(`\x1b[30m${argument}\x1b[0m`);
-            } else {
-                process.stdout.write(argument)
+    return (lines, options) => {
+        lines.forEach((line) => {
+            if (line.length) {
+                line += '\n';
+                if (['error', 'fatal'].includes(level.toLowerCase())) {
+                    process.stderr.write(`\x1b[31m${line}\x1b[0m`)
+                } else {
+                    if (level.toLowerCase() === 'debug') {
+                        process.stdout.write(`\x1b[30m${line}\x1b[0m`);
+                    } else {
+                        // console.log('line_here', line);
+                        process.stdout.write(line);
+                    }
+                }
             }
-        }
+        });
     };
 };
 
-const write = (...args) => {
-    const print = usePrint(args[2]);
+const buildLarge = (argument) => {
+    if (argument instanceof Error) {
+        return buildError(argument);
+    } else if (typeof argument === 'object') {
+        return buildObject(argument);
+    } else if (typeof argument === 'function') {
+        return buildFunction(argument);
+    }
+};
 
-    const writeObject = (argument) => {
+const buildError = (argument) => {
+    return argument.stack;
+};
 
-        const strArg = JSON.stringify(argument);
-        const parsedStrArg = strArg.split('');
-    
-        print('\n');
-    
-        let tabCount = 0;
-        let spaceReady = false;
-        let key = false;
-    
-        const processStrArr = (strArray) => {
-            if (!strArray.length) return;
-    
-            const char = strArray[0];
-    
-            switch (char) {
-                case '{':
-                case '[':
-                    if (strArray[1] === '}' || strArray[1] === ']') {
-                        print(`${char}${strArray[1]}`);
-                        strArray = strArray.slice(1);
-                    } else {
-                        print(char);
-                        tabCount++;
-                        spaceReady = true;
-                        key = true;
-                    }
-                    break;
-                case ',':
-                    print(`${char} `);
+const buildFunction = (argument) => {
+    return argument;
+};
+
+const buildObject = (argument) => {
+    let objectStringified = '';
+
+    const strArg = JSON.stringify(argument);
+    const parsedStrArg = strArg.split('');
+
+    objectStringified += '\n';
+
+    let tabCount = 0;
+    let spaceReady = false;
+    let key = false;
+
+    const processStrArr = (strArray, objString) => {
+        if (!strArray.length) return objString;
+
+        const char = strArray[0];
+
+        switch (char) {
+            case '{':
+            case '[':
+                if (strArray[1] === '}' || strArray[1] === ']') {
+                    objString += `${char}${strArray[1]}`;
+                    strArray = strArray.slice(1);
+                } else {
+                    objString += char;
+                    tabCount++;
                     spaceReady = true;
                     key = true;
-                    break;
-                case '}':
-                case ']':
-                    tabCount--;
-                    print(`\n${writeTabs(tabCount)}${char}`);
-                    break;
-                case ':':
-                    key = false;
-                    print(`${char} `);
-                    break;
-                case '"':
-                    if (!key) print(char);
-                    break;
-                default:
-                    const newLine = (spaceReady) ? `\n${writeTabs(tabCount)}` : '';
-                    print(`${newLine}${char}`);
-                    spaceReady = false;
-                    break;
-            }
-    
-            processStrArr(strArray.slice(1));
+                }
+                break;
+            case ',':
+                objString += `${char} `;
+                spaceReady = true;
+                key = true;
+                break;
+            case '}':
+            case ']':
+                tabCount--;
+                objString += `\n${writeTabs(tabCount)}${char}`;
+                break;
+            case ':':
+                key = false;
+                objString += `${char} `;
+                break;
+            case '"':
+                if (!key) objString += char;
+                break;
+            default:
+                const newLine = (spaceReady) ? `\n${writeTabs(tabCount)}` : '';
+                objString += `${newLine}${char}`;
+                spaceReady = false;
+                break;
         }
-    
-        processStrArr(parsedStrArg);
-    };
-    
-    const writeFunction = (argument) => {
-        print(argument);
-    };
 
-    const writeError = (argument) => {
-        print(argument.stack);
-    };
-    
-    const writeLarge = (argument) => {
-        if (argument instanceof Error) {
-            writeError(argument);
-        } else if (typeof argument === 'object') {
-            writeObject(argument);
-        } else if (typeof argument === 'function') {
-            writeFunction(argument);
+        return processStrArr(strArray.slice(1), objString);
+    }
+
+    return processStrArr(parsedStrArg, objectStringified);
+};
+
+const formatter = (args) => {
+    const lines = [];
+    let workingLine = [];
+
+    const addToLine = (str) => {
+        if (typeof str === 'string' && str.includes('\n')) {
+            const lineBreaks = str.split('\n');
+            lineBreaks.forEach((line) => {
+                workingLine.push(line);
+                const stringifiedLine = workingLine.join(' ');
+                lines.push(stringifiedLine);
+                workingLine = [];
+            });
+        } else {
+            workingLine.push(str);
         }
-    };
+    }
 
     args.forEach((argument, idx) => {
         if (typeof argument === 'object' || typeof argument === 'function') {
-            writeLarge(argument);
+            addToLine(buildLarge(argument));
         } else if (idx === 2) {
             const color = colors[argument.toLowerCase()];
-            print(`${color}${argument}\x1b[0m `);
+            addToLine(`${color}${argument}\x1b[0m`);
         } else {
-            print(`${argument} `);
+            addToLine(argument);
         }
     });
-    print('\n');
+
+    const stringifiedLine = workingLine.join(' ');
+    lines.push(stringifiedLine);
+
+    return lines;
+}
+
+const write = (...args) => {
+    const print = usePrint(args[2]);
+    const lines = formatter(args);
+
+    print(lines);
 };
 
 module.exports = write;
